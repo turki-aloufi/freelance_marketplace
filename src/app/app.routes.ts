b0ff0927ea2795fd507674389ecf72854a2086d5
turki-aloufi <turkialoufi44@gmail.com>
2025-05-07 10:53:24 +0300

    finished client approved projects page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignInComponent } from './features/auth/sign-in/sign-in.component';
 import { HomeComponent } from './features/home/home.component';
 import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
 import { ClientProjectsComponent } from './features/client-projects/client-projects.component';
+import { ClientApprovedProjectsComponent } from './features/client-approved-projects/client-approved-projects.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'client/projects', component: ClientProjectsComponent },
+  { path: 'client/projects/approved', component: ClientApprovedProjectsComponent },
   { path: '**', redirectTo: '/home' },
 ];
