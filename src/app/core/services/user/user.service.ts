6d755849527b592c66469e5466535bfd21a1b7d0
Osama Alhejaily <osamahhumdan@gmail.com>
2025-05-10 22:47:46 +0300

    Fixed issue

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom, switchMap, take, Observable } from 'rxjs';
+import { Auth, user, User } from '@angular/fire/auth';
+
 export interface BalanceChangeDto {
   amount: number;
 }
@@ -9,14 +11,41 @@ export interface BalanceChangeDto {
 })
 export class UserService {
   private apiUrl = 'http://localhost:5021/api/Auth'; 
+  private user$: Observable<User | null>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: Auth) {
+    this.user$ = user(this.auth);
+
+  }
+
+    private getAuthToken(): Observable<string> {
+        return this.user$.pipe(
+          take(1),
+          switchMap(user => {
+            if (!user) throw new Error('No authenticated user');
+            return user.getIdToken();
+          })
+        );
+      }
 
   getUserProfile(userId: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/users/${userId}`);
   }
-  changeUserBalance(userId: string, dto: BalanceChangeDto): Observable<any> {
-    return this.http.put(`${this.apiUrl}/users/${userId}/balance/change`, dto);
+
+  async updateBalance(amount: number): Promise<void> {
+    const token = await firstValueFrom(this.getAuthToken());
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    await firstValueFrom(
+      this.http.put(`${this.apiUrl}/balance/change`,
+        { amount },
+        { headers }
+      )
+    );
   }
+
+  getCurrentUserId(): string | null {
+    return this.auth.currentUser?.uid || null;
+  }
+  
  
 }
