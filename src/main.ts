de6266a11ea1c1e9892182e84bf014d8e083d463
Razan Al-ahmadi <a.razan@obeikan.com.sa>
2025-05-13 00:14:52 +0300

    Add more bugsnag setting

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+//Bugsnag
+import Bugsnag from '@bugsnag/js';
+Bugsnag.start({ apiKey: '57275aeb5e5f2cfbc08f4e8df830e4a8' });
+console.log('✅ Bugsnag has been started');
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
