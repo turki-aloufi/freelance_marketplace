3e5fa2ea8856754a0ff4e4c59e3ec359a0c91ee2
shadiaal <shadiaalmtiri@gmail.com>
2025-05-14 21:51:50 +0300

    clean code

--- src/app/features/project/project-detail/project-detail.component.ts
+++ src/app/features/project/project-detail/project-detail.component.ts
@@ -47,25 +47,22 @@ export class ProjectDetailComponent implements OnInit {
 
     this.projectService.getProjectById(this.projectId).subscribe(project => {
       this.project = project;
-      console.log('Project Data:', project);
+    
 
       const currentUser = this.authService.user$.value;
       const token = currentUser?.getIdToken();
-      console.log('token:', token);  
-      console.log('Current User:', currentUser);
-      console.log('PostedBy:', project.clientId);
-
+    
       if (currentUser && project.clientId && project.clientId === currentUser.uid) {
         this.isProjectOwner = true;
       } else {
         this.isProjectOwner = false;
       }
     
-      console.log('isProjectOwner:', this.isProjectOwner);
+  
       
       // Check if the user is logged in and is not the project owner
       this.isLoggedInNotOwner = currentUser && !this.isProjectOwner;
-      console.log('isLoggedInNotOwner:', this.isLoggedInNotOwner);
+    
 
       if (project.requiredTasks && typeof project.requiredTasks === 'string') {
         this.requiredTasksArray = project.requiredTasks.split(',').map(task => task.trim());
@@ -147,7 +144,7 @@ export class ProjectDetailComponent implements OnInit {
     freelancerPhoneNumber: proposal.freelancerPhoneNumber,
   };
 
-  console.log("the body: ", model);
+
 
   this.projectService.assignProject(this.projectId, model).subscribe(
     () => {
