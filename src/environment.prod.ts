dbcf55bf5296dbca7b6aa8c8e4d3c177d6ce1ab3
Areej Shareefi <aomsh1420@gmail.com>
2025-05-14 13:12:06 +0300

    configure for prodction

---
+++ src/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  "stripe": {
+    "publishableKey": "pk_test_51RJiMwIyGkuZhmdotUorbeSaWwVa0PayGNnEmVCChQ5fjcxqJebJx0PlOWAtOvnxKObMMejXscYHA4pwVEhnnk1o00FGE6WjKu",
+   },
+  firebaseConfig: {
+    apiKey: "AIzaSyBi4IxieY_oEjIHVI7Cg7WxOUrS45qiMLg",
+    authDomain: "freelance-marketplace-caf38.firebaseapp.com",
+    projectId: "freelance-marketplace-caf38",
+    storageBucket: "freelance-marketplace-caf38.firebasestorage.app",
+    messagingSenderId: "966667474169",
+    appId: "1:966667474169:web:6429e4fe6bdc608fd4fe28",
+    measurementId: "G-4D2194RL6W"
+  },
+
+};
